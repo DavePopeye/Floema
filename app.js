6696c0a6328262cd69636d8884bae578c5f4c6c8
DavidePopeye <davidepapa1988@gmail.com>
2021-10-08 10:09:53 +0200

    commit

--- app.js
+++ app.js
@@ -1,27 +1,73 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const path =require('path')
 const port = 3000
 
+const Prismic = require('@prismicio/client');
+const PrismicDOM = require('prismic-dom');
+
+const initApi= req => {
+  return Prismic.getApi(process.env.PRISMIC_ENDPOINT, {
+    accessToken: process.env.PRISMIC_ACCESS_TOKEN,
+    req
+  });
+}
+
+const handleLinkResolver = doc => {
+
+  /*if (doc.type === 'page') {
+    return '/page/' + doc.uid;
+  } else if (doc.type === 'blog_post') {
+    return '/blog/' + doc.uid;
+  }
+  */
+  return '/';
+}
+
+app.use((req, res, next) => {
+  res.locals.ctx = {
+    endpoint: process.env.PRISMIC_ENDPOINT,
+    linkResolver: handleLinkResolver,
+  };
+  // add PrismicDOM in locals to access them in templates.
+  res.locals.PrismicDOM = PrismicDOM;
+  next();
+});
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   res.render('pages/home')
 })
 
-app.get('/about', (req, res) => {
-  res.render('pages/about')
+app.get('/about', async (req, res) => {
+  initApi(req).then(api => {
+    api.query(Prismic.Predicates.any('document.type', ['meta', 'about'])).then(response => {
+
+      const {results} = response
+      const [meta, about] = results
+
+      console.log(meta, about)
+
+      res.render('pages/about'), {
+        about,
+        meta
+      };
+    });
+  });
 })
 
 app.get('/detail/:', (req, res) => {
   res.render('pages/detail')
 })
 
-app.get('/collections', (req, res) => {
-  res.render('pages/collections')
+app.get('/collection', async (req, res) => {
+  res.render('pages/collection')
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`app listening at http://localhost:${port}`)
 })
